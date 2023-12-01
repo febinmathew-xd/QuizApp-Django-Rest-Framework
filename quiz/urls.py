f24f1c83c375b998e41332640be37bab002ed076
febin <febnmathew98@gmail.com>
2023-12-01 14:56:20 +0530

    end points added

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import active
+from .views import get_all_questions, get_active_category, get_history_category, edit_active_status
 
 
 urlpatterns = [
-    path("active/", active),
+    path("questions/<int:pk>/", get_all_questions),
+    path('category/active/', get_active_category),
+    path('category/history/', get_history_category),
+    path('category/set/inactive/<int:pk>/',edit_active_status )
 ]
