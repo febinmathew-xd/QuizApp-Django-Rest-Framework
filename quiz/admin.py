ab309665793d089333b4839ebfe92dd2e8ca70ae
febin <febnmathew98@gmail.com>
2023-12-01 14:57:25 +0530

    admin panel customization

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,15 @@
+# admin.py
 from django.contrib import admin
+from .models import Question, Option, Category
 
-# Register your models here.
+class OptionInline(admin.TabularInline):
+    model = Option
+    extra = 4 # Set the number of options you want to add at once
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [OptionInline]
+
+admin.site.register(Question, QuestionAdmin)
+
+admin.site.register(Category)
+
